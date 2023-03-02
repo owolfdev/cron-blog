3d9537f416db7caecb445c6b342e04d7b68dedda
owolfdev <owolfdev@gmail.com>
2023-03-03 03:52:48 +0700

    add styling

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -6,10 +6,10 @@ function Blog({ posts }: any) {
       <div>
         {posts.map((post: any) => {
           return (
-            <div key={post._id}>
-              <h3>{post.title}</h3>
+            <div className="mb-2" key={post._id}>
+              <h3 className="text-2xl font-bold">{post.title}</h3>
               <p>{post.content}</p>
-              <p>{post.date}</p>
+              <p className="mt-1 text-sm text-gray-400">{post.date}</p>
             </div>
           );
         })}
