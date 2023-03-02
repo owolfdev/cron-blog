32fe51753d9ceaa1c5896d1ab12ee194a8f91afc
owolfdev <owolfdev@gmail.com>
2023-03-03 03:58:48 +0700

    add styling

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 
 function Blog({ posts }: any) {
+  const formatDate = (date: any) => {
+    const dateObj = new Date(date);
+    const year = dateObj.getUTCFullYear();
+    const month = String(dateObj.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(dateObj.getUTCDate()).padStart(2, "0");
+    const formattedDate = `${year}-${month}-${day}`;
+    return formattedDate;
+  };
+
   return (
     <div>
       <div>
         {posts.map((post: any) => {
           return (
-            <div className="mb-2" key={post._id}>
+            <div className="p-3" key={post._id}>
               <h3 className="text-2xl font-bold">{post.title}</h3>
               <p>{post.content}</p>
-              <p className="mt-1 text-sm text-gray-400">{post.date}</p>
+              <p className="mt-1 text-sm text-gray-400">
+                {formatDate(post.date)}
+              </p>
             </div>
           );
         })}
